eead6df7c914745b18ef827be3c96bb3d865d9b7
Carlos Eduardo Ema <CEma@futurebrand.com>
2025-05-06 16:47:25 -0300

    feat: footer

---
+++ next-tailwind/components/footer/footer.tsx
@@ -0,0 +1,93 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="w-full bg-green">
+      <footer className="container grid grid-cols-1 py-7">
+        <div className="cima border-b-2 border-b-white flex flex-col md:flex-row justify-between items-start md:items-end gap-4 md:gap-0">
+          <div className="image col-start-1 py-2">
+            <Image
+              className="revo"
+              src="/revologo.svg"
+              alt="revologo.svg logo"
+              width={171}
+              height={143}
+              priority
+            />
+          </div>
+          <div className="tell text-revolime text-2xl">
+            11 98682-0203
+          </div>
+        </div>
+
+        <div className="baixo text-white flex flex-col md:flex-row justify-between gap-6 pt-4">
+          <div>
+            <ul className="flex flex-col gap-1">
+              <li>&copy; 2023 All right reserved.</li>
+              <li className="flex items-center gap-3">
+                Site by
+                <Image
+                  className="FutureBrand"
+                  src="/icon/Logofb.svg"
+                  alt="FutureBrand logo"
+                  width={95}
+                  height={12}
+                  priority
+                />
+              </li>
+            </ul>
+          </div>
+
+          <div className="social-icons flex flex-col md:flex-row items-start md:items-center gap-2">
+            <p>Social media</p>
+            <div className="flex gap-2">
+              <Image
+                className="linkedin"
+                src="/icon/LI.svg"
+                alt="Icone do LinkedIn"
+                width={32}
+                height={32}
+                priority
+              />
+              <Image
+                className="instagram"
+                src="/icon/INS.svg"
+                alt="Icone do Instagram"
+                width={32}
+                height={32}
+                priority
+              />
+              <Image
+                className="spotify"
+                src="/icon/SP.svg"
+                alt="Icone do Spotify"
+                width={32}
+                height={32}
+                priority
+              />
+              <Image
+                className="youtube"
+                src="/icon/YT.svg"
+                alt="Icone do Youtube"
+                width={32}
+                height={32}
+                priority
+              />
+              <Image
+                className="facebook"
+                src="/icon/FB.svg"
+                alt="Icone do Facebook"
+                width={32}
+                height={32}
+                priority
+              />
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer
