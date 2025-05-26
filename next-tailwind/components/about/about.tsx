1e8840cb668415e055cc656df1fdc5b61739a2d2
Carlos Eduardo Ema <CEma@futurebrand.com>
2025-05-26 13:16:16 -0300

    fix: layout problems

--- next-tailwind/components/about/about.tsx
+++ next-tailwind/components/about/about.tsx
@@ -21,16 +21,18 @@ const About = () => {
       },
     },
   };
+
   return (
-    <div className="w-full h-screen bg-half py-44 text-revogreen">
-      <motion.div
-        className="container-small grid grid-cols-2"
-        initial="offscreen"
-        whileInView="onscreen"
-        viewport={{ amount: 0.4, once: true }}
-        variants={cardVariants}
-      >
-        <div className="container-small col-start-1">
+    <section className="block-about flex-col md:flex-row text-revogreen overflow-hidden relative">
+      <div className="container-small grid grid-cols-1 md:grid-cols-10 gap-x-10 gap-y-40 py-32">
+        {/* Conteúdo "Quem Somos" animado */}
+        <motion.div
+          className="md:col-span-4"
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{ amount: 0.1, once: true }}
+          variants={cardVariants}
+        >
           <h2 className="text-2xl sm:text-3xl pb-7 uppercase font-bold">
             Quem Somos
           </h2>
@@ -39,10 +41,17 @@ const About = () => {
             por meio da nossa expertise e parceiros estratégicos que possuem
             vasto histórico de atuação no Setor de Energia Renovável.
           </p>
-        </div>
+        </motion.div>
 
+        {/* Fundo fixo + conteúdo animado */}
         <div className="green-bg md:col-span-4 md:col-start-7 relative md:static">
-          <div className="titulo ">
+          <motion.div
+            className="titulo"
+            initial="offscreen"
+            whileInView="onscreen"
+            viewport={{ amount: 0.1, once: true }}
+            variants={cardVariants}
+          >
             <div className="w-auto">
               <Image
                 className="h-16 w-auto object-contain"
@@ -57,8 +66,15 @@ const About = () => {
             <h2 className="text-2xl sm:text-3xl uppercase font-bold">
               Parceria estratégica
             </h2>
-          </div>
-          <p className="sm:text-2xl">
+          </motion.div>
+
+          <motion.p
+            className="sm:text-2xl"
+            initial="offscreen"
+            whileInView="onscreen"
+            viewport={{ amount: 0.1, once: true }}
+            variants={cardVariants}
+          >
             A REVO Energia é Parceira Oficial da HUAWEI, líder global de
             soluções de Tecnologia da Informação e Comunicação (TIC) e Energia
             Renovável. A divisão solar da Huawei opera em mais de 170 países em
@@ -66,10 +82,10 @@ const About = () => {
             serviços de alta qualidade, incluindo inversores de string,
             inversores centrais, soluções PV inteligentes e sistemas de
             monitoramento e controle.
-          </p>
+          </motion.p>
         </div>
-      </motion.div>
-    </div>
+      </div>
+    </section>
   );
 };
 
